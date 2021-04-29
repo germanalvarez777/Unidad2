0865d8f1227ec24165182b86bab86add090a24d0
German Alvarez Manzini <71088676+germanalvarez777@users.noreply.github.com>
2021-04-29 11:51:51 -0300

    Add files via upload

--- claseMain.py
+++ claseMain.py
@@ -5,21 +5,20 @@ def test ():
     print("TEST".center(40, '='))
     mpTest = ManejadorProyecto()
     mpTest.testListaProyecto()
+    print("Mostramos el listado de proyectos: ")
+    mpTest.mostrarListadoProy()
 
     mipTest = ManejadorIntegProyecto()
     mipTest.testListaInteg()
     print("Mostramos el listado de integrantes: ")
     mipTest.mostrarListadoInteP()
 
-    mpTest.calcularPuntaje(mipTest)
-    
-    print("\nMostramos el listado de Proyectos en forma ordenada(ascendente) por puntaje\n")
-    mpTest.listadoOrdenado ()
 
 if __name__ == '__main__':
 
     test()
-    print("Testeo de datos ha finalizado".center(30, '-'))
+    print("Testeo de datos ha finalizado".center(40, '-'))
+    input("Presione enter para continuar: ")
     mp = ManejadorProyecto()
     mp.testListaProyecto()
 
